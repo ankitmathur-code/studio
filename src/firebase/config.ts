c69fe3690194a66c7b531072a1a1fa344dcc3c2b
Ankit Mathur <ankit.mathur@monks.com>
2026-03-11 11:55:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-925736919-4cfea",
+  "appId": "1:433406846699:web:337f8db334d3f6f7a140ae",
+  "apiKey": "AIzaSyAT7MAbiogpRHlNAiKMPZDt8RQ7e8X8s6k",
+  "authDomain": "studio-925736919-4cfea.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "433406846699"
+};
